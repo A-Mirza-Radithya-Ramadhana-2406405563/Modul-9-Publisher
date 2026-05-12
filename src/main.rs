7af4af53b5ba8a213c3b3dd9f2be0fd7b0c654f7
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-05-12 19:06:26 +0700

    fix: change wrong NPM

--- src/main.rs
+++ src/main.rs
@@ -24,18 +24,18 @@ impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
 fn main() {
     let mut p = CrosstownBus::new_queue_publisher("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
     _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
-        user_id: "1".to_owned(), user_name: "129500004y-Amir".to_owned() 
+        user_id: "1".to_owned(), user_name: "2406405563-Amir".to_owned() 
     });
     _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
-        user_id: "2".to_owned(), user_name: "129500004y-Budi".to_owned() 
+        user_id: "2".to_owned(), user_name: "2406405563-Budi".to_owned() 
     });
     _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
-        user_id: "3".to_owned(), user_name: "129500004y-Cica".to_owned() 
+        user_id: "3".to_owned(), user_name: "2406405563-Cica".to_owned() 
     });
     _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
-        user_id: "4".to_owned(), user_name: "129500004y-Dira".to_owned() 
+        user_id: "4".to_owned(), user_name: "2406405563-Dira".to_owned() 
     });
     _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
-        user_id: "5".to_owned(), user_name: "129500004y-Emir".to_owned() 
+        user_id: "5".to_owned(), user_name: "2406405563-Emir".to_owned() 
     });
 }
